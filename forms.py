d7f40d503d8b1e88daae631afb50e2d271c45598
William Spanfelner <wil-1-am@outlook.com>
2022-07-05 21:37:27 +0300

    Add a LoginForm object

--- forms.py
+++ forms.py
@@ -18,3 +18,9 @@ class RegisterForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("SIGN ME UP!")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("LET ME IN!")
