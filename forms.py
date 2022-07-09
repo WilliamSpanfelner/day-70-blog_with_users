11d89bbb88ec47fe8dc18f7d337416aadaf782a7
William Spanfelner <wil-1-am@outlook.com>
2022-07-09 15:41:17 +0300

    Add new form to collect comments.

--- forms.py
+++ forms.py
@@ -5,6 +5,11 @@ from flask_ckeditor import CKEditorField
 
 
 # WTForm
+class CommentForm(FlaskForm):
+    comment = CKEditorField("Comment", validators=[DataRequired()])
+    submit = SubmitField("SUBMIT COMMENT")
+
+
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
