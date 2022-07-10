ccc2e365c2d6a4c610b7dfcd8b13e479f7da68cf
William Spanfelner <wil-1-am@outlook.com>
2022-07-10 21:38:26 +0300

    Update app.config to use Postgres if available and sqlite locally. Add a new environment variable to heroku

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ from functools import wraps
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///blog.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
