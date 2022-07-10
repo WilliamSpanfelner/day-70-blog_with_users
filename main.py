6843f579b8e5bd77b169636fd1060ac058f8d209
William Spanfelner <wil-1-am@outlook.com>
2022-07-10 22:06:13 +0300

    Update for connecting to Heroku Postgres services using SQLAlchemy >= 1.4.x

--- main.py
+++ main.py
@@ -12,13 +12,17 @@ from flask_gravatar import Gravatar
 from functools import wraps
 import os
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+uri = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
